498da644afe4f5d2a9ccc296dd3ed39b94ae36a4
KNiC.xo <ntcaliwag@hotmail.com>
2023-10-03 09:24:59 -0700

    Edit and Delete buttons show when hovering note

--- note-taking.js
+++ note-taking.js
@@ -1,16 +1,37 @@
 const createNoteBtn = document.getElementById("createNote");
 const notesDiv = document.getElementById("notesDiv");
+
 let numNotes = 0;
 let noteArr = [];
+
+let isEditing = false;
 
 createNoteBtn.addEventListener('click', createNote);
 
 function createNote() {
   numNotes++;
 
-  notesDiv.innerHTML = `<div class="noteElement note${numNotes}"><textarea class="textArea${numNotes}" cols="35" rows="10"></textarea> <button class="save saveBtn${numNotes}">Save</button> <button class="delete deleteBtn${numNotes}">Delete</button></div>` + notesDiv.innerHTML;
+  notesDiv.innerHTML = `<div class="noteElement note${numNotes}" data-note-number="${numNotes}"><textarea class="textArea${numNotes}" cols="35" rows="10"></textarea> <button class="edit editBtn${numNotes}">Save</button> <button class="delete deleteBtn${numNotes}">Delete</button></div>` + notesDiv.innerHTML;
 
-  document.querySelector(`.saveBtn${numNotes}`).addEventListener('click', () => {
+  document.querySelectorAll(`.noteElement`).forEach((note) => {
+    note.addEventListener('mouseover', () => {
+      const noteNum = note.dataset.noteNumber;
+      if (!isEditing) {
+        document.querySelector(`.editBtn${noteNum}`).style.visibility = "visible";
+        document.querySelector(`.deleteBtn${noteNum}`).style.visibility = "visible";
+      }
+    });
+  
+    note.addEventListener('mouseout', () => {
+      const noteNum = note.dataset.noteNumber;
+      if (!isEditing) {
+        document.querySelector(`.editBtn${noteNum}`).style.visibility = "hidden";
+        document.querySelector(`.deleteBtn${noteNum}`).style.visibility = "hidden";
+      }
+    });
+  });
+
+  document.querySelector(`.editBtn${numNotes}`).addEventListener('click', () => {
     saveNote(numNotes);
   });
 
@@ -18,8 +39,8 @@ function createNote() {
     deleteNote(numNotes);
   });
 
+  isEditing = true;
   createNoteBtn.disabled = true;
-
   loadText();
 }
 
@@ -29,11 +50,11 @@ function saveNote(noteNum) {
   noteArr.push(note.value);
   noteArr.forEach(index => console.log(index));
 
+  document.querySelector(`.editBtn${noteNum}`).style.visibility = "hidden";
+  document.querySelector(`.deleteBtn${noteNum}`).style.visibility = "hidden";
 
-  document.querySelector(`.saveBtn${noteNum}`).style.display = "none";
-  document.querySelector(`.deleteBtn${noteNum}`).style.display = "none";
+  isEditing = false;
   createNoteBtn.disabled = false;
-
 }
 
 function deleteNote(noteNum) {
